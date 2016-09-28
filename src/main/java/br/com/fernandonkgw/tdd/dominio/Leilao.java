4d7c6dfb5ce5c329ac4006e3a1b25a92f64d0216
Fernando Nakagawa <fernandonkgw@gmail.com>
2016-09-28 11:37:50 -0300

    Ajustes na classe para modulo de mocks

--- src/main/java/br/com/fernandonkgw/tdd/dominio/Leilao.java
+++ src/main/java/br/com/fernandonkgw/tdd/dominio/Leilao.java
@@ -1,34 +1,45 @@
 package br.com.fernandonkgw.tdd.dominio;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.List;
 
 public class Leilao {
 
 	private String descricao;
+	private Calendar data;
 	private List<Lance> lances;
+	private boolean encerrado;
+	private int id;
 
 	public Leilao(String descricao) {
 		this.descricao = descricao;
 		this.lances = new ArrayList<Lance>();
 	}
 
+	public Leilao(String descricao, Calendar data) {
+		this.descricao = descricao;
+		this.data = data;
+		this.lances = new ArrayList<Lance>();
+	}
+
 	public void propoe(Lance lance) {
-		
+
 		if (lances.isEmpty() || podeDarLance(lance.getUsuario())) {
 			lances.add(lance);
 		}
 	}
 
 	private boolean podeDarLance(Usuario usuario) {
-		return !ultimoLanceDado().getUsuario().equals(usuario) && qtdeDeLancesDo(usuario) < 5;
+		return !ultimoLanceDado().getUsuario().equals(usuario)
+				&& qtdeDeLancesDo(usuario) < 5;
 	}
 
 	private int qtdeDeLancesDo(Usuario usuario) {
 		int total = 0;
 		for (Lance l : lances) {
-			if(l.getUsuario().equals(usuario)) {
+			if (l.getUsuario().equals(usuario)) {
 				total++;
 			}
 		}
@@ -38,7 +49,7 @@ public class Leilao {
 	public Lance primeiroLanceDado() {
 		return lances.get(0);
 	}
-	
+
 	public Lance ultimoLanceDado() {
 		return lances.get(lances.size() - 1);
 	}
@@ -60,10 +71,39 @@ public class Leilao {
 
 	private Lance ultimoLanceDo(Usuario usuario) {
 		Lance ultimo = null;
-		for(Lance lance : this.lances) {
-			if (lance.getUsuario().equals(usuario)) ultimo = lance;
+		for (Lance lance : this.lances) {
+			if (lance.getUsuario().equals(usuario))
+				ultimo = lance;
 		}
 		return ultimo;
+	}
+
+	public boolean isEncerrado() {
+		return encerrado;
+	}
+
+	public void encerra() {
+		this.encerrado = true;
+	}
+
+	public Calendar getData() {
+		return data;
+	}
+
+	public void setData(Calendar data) {
+		this.data = data;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setEncerrado(boolean encerrado) {
+		this.encerrado = encerrado;
 	}
 
 }
--- src/test/java/br/com/fernandonkgw/tdd/builder/CriadorDeLeilao.java
+++ src/test/java/br/com/fernandonkgw/tdd/builder/CriadorDeLeilao.java
@@ -1,24 +1,52 @@
 package br.com.fernandonkgw.tdd.builder;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
 import br.com.fernandonkgw.tdd.dominio.Lance;
 import br.com.fernandonkgw.tdd.dominio.Leilao;
 import br.com.fernandonkgw.tdd.dominio.Usuario;
 
 public class CriadorDeLeilao {
 
-	private Leilao leilao;
+	
+	private String descricao;
+	private Calendar data;
+	private List<Lance> lances;
+	private boolean encerrado;
 
+	public CriadorDeLeilao() {
+		this.data = Calendar.getInstance();
+		lances = new ArrayList<>();
+	}
+	
 	public CriadorDeLeilao para(String descricao) {
-		this.leilao = new Leilao(descricao);
+		this.descricao = descricao;
 		return this;
 	}
 
+	public CriadorDeLeilao naData(Calendar data) {
+		this.data = data;
+		return this;
+	}
+	
 	public CriadorDeLeilao lance(Usuario usuario, double valor) {
-		leilao.propoe(new Lance(usuario, valor));
+		lances.add(new Lance(usuario, valor));
+		
 		return this;
 	}
 
+	public CriadorDeLeilao encerrado() {
+		this.encerrado = true;
+		return this;
+	}
+	
 	public Leilao constroi() {
+		Leilao leilao = new Leilao(descricao, data);
+		for (Lance lanceDado : lances) leilao.propoe(lanceDado);
+		if(encerrado) leilao.encerra();
+		
 		return leilao;
 	}
 
